7271afeb63dc86995d8cf5d7031e4b886a0e298a
visionxStudio <manish.rajak2055@gmail.com>
2021-04-04 01:53:55 +0545

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-// import 'package:uberclone/screens/homepage.dart';
-import 'package:uberclone/screens/loginpage.dart';
+import 'package:uberclone/views/loginpage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
